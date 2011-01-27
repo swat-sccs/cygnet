4806083fb99597062173ac1bddf31d92e8a1fb91
Nick Felt <nfelt1@sccs.swarthmore.edu>
2011-01-27 00:00:51 -0500

    Moved libXML.js to livecygnet.js to facilitate merging two files.

--- new/livecygnet.js
+++ new/livecygnet.js
@@ -1,161 +1,45 @@
-var timer = null;
+/*
+ * importXML - handle the messiness of creating the XMLHttpRequest
+ * across different browser types, and insulate the user from nasty
+ * errors.
+ *
+ * @param[in] webservice the URL of the XML document to fetch (can include query-string)
+ * @param[in] callback   the function co call once the XML fetch is completed
+ * @param[in] noXMLloc   a page to redirect to if the browser doesn't support XMLHttpRequests
+ */ 
+function importXML(webservice, callback, noXMLloc) {
 
-var starttime = 0;
-
-var lastsearch = "";
-
-function doSearch() {
-    clearTimeout(timer);
-    starttime = (new Date()).getTime();
+    var workers = [
+        "MSXML2.XMLHTTP.5.0", 
+        "MSXML2.XMLHTTP.4.0", 
+        "MSXML2.XMLHTTP.3.0", 
+        "Microsoft.XMLHTTP"
+        ]; 
     
-    var searchterms = document.getElementById("terms").value;
-    lastsearch = searchterms;
-    
-    updateBookmarkLink();
-    
-    // Ignore empty searches, instead clear the results area
-    if (searchterms == "") {
-        document.getElementById("results").innerHTML = "";
-            
-    // Do search if different from search just performed
-    } else if (!sameSearch(searchterms)) {
-        document.getElementById("spinner").src = "spinner.gif";
-        importXML("cygnetxml.py?terms=" + searchterms, displayResults, "index.php");
-    }
-}
+    var xmlReq = null;
 
-function updateBookmarkLink() {
-    var url = 'index.php';
-    if (lastsearch != "") {
-        url += "?terms=" + lastsearch;
-    }
-    document.getElementById("bookmarkurl").href = url;
-}
-
-function displayResults(xmlDoc) {
-
-    if (xmlDoc.status != 200) {
-		alert("HTTP Status: " + xmlDoc.status + " = " + xmlDoc.statusText);
-		die();
-    }
-
-    if (xmlDoc.responseXML == null) {
-		alert("Error: xmlDoc.responseXML is null");
-		alert("response text: " + xmlDoc.responseText);
-		throw("Error: xmlDoc.responseXML is null");
-		die();
-    }
-
-    var div = document.getElementById("results");
-    var newHTML = "";
-    var docElement = xmlDoc.responseXML.documentElement;
-    var textNodeType = 3;
-    var results = 0;
-
-    div.innerHTML = "";
-    
-    
-    if (!docElement.hasChildNodes()) {
-        newHTML += "Search for \"" + lastsearch + "\" found no matches";
-    } else {
-        var node = docElement.firstChild;
-        
-        var people = docElement.childNodes;
-
-        
-        
-        newHTML += '<table border="0" cellpadding="0" cellspacing="0">';
-        
-        for (var i=0; i<people.length; i++) {
-            var person = people[i];
-
-            if (person.nodeType == textNodeType) {
-                continue;
-            }
-            
-            var nodeDict = {};
-            nodeDict["first"] = "";
-            nodeDict["middle"] = "";
-            nodeDict["last"] = "";
-            nodeDict["address"] = "";
-            nodeDict["phone"] = "";
-            nodeDict["email"] = "";
-            nodeDict["class"] = "";
-            
-            var fields = person.childNodes;
-            
-            for (var j=0; j< fields.length; j++) {
-                var field = fields[j];
-
-                if (field.nodeType == textNodeType) {
-                    continue;
-                }
-
-                nodeDict[field.nodeName] = field.firstChild.nodeValue;
-            }
-
-            // add leading "x " to 4-digit phone numbers
-            if (nodeDict["phone"] && nodeDict["phone"].length == 4) {
-                nodeDict["phone"] = "x " + nodeDict["phone"];
-            }
-            
-            var col = results % 4;
-            if (col == 0) {
-                if (results > 0) {
-                    newHTML += "</tr>";
-                }
-                newHTML += "<tr>";
-            }
-
-            newHTML += "<td>";
-            newHTML += '<img src="photos/' + nodeDict["class"] + '/' + nodeDict["email"] +
-                       '.jpg" alt="' + nodeDict["first"] + " " + nodeDict["last"] + '"/><br/>';
-            newHTML += nodeDict["first"] + " " + nodeDict["middle"] + " " + nodeDict["last"] + "<br/>";
-            newHTML += '<span style="font-size:smaller">';
-            newHTML += nodeDict["address"] + " (" + nodeDict["phone"] + ")<br/>";
-            newHTML += nodeDict["class"] + " / " + nodeDict["email"] + "<br/>";
-            newHTML += "</span></td>";
-            
-            results ++;
+    try {
+        xmlReq = new XMLHttpRequest();
+    } catch (error) {
+        for (var i=0; i<workers.length; i++) {
+            try {
+                xmlReq = new ActiveXObject(workers[i]);
+                
+                break; // exit the loop
+            } catch (ignored) { }
         }
-
-        var col = (results % 4);
-        if (col != 0 && results > 4) {
-            for (var i=0; i<(4 - col); i++) {
-                newHTML += "<td>&nbsp;</td>";
-            }
-        }
-
-        newHTML += "</table>";
     }
-    
-    div.innerHTML = "Search for \"" + lastsearch + "\" returned " + results + " results.<br/><br/>" + newHTML;
 
-    // now clear the message
-    document.getElementById("spinner").src = "spinner-stopped.gif";
-    
-    var endtime = (new Date()).getTime();
-    var timediff = endtime - starttime;
-    document.getElementById("timevalue").innerHTML = timediff + " ms";
+    if (xmlReq == null || xmlReq == undefined) {
+        window.location = noXMLloc;
+    }
+            
+    xmlReq.open("get", webservice, true);
+    xmlReq.onreadystatechange = function() {
+	if (xmlReq.readyState == 4) {
+	    callback(xmlReq);
+
+	}
+    };
+    xmlReq.send(null);
 }
-
-var TYPEDELAY = 300; // In milliseconds.
-
-function callSearch(noDelay) {
-    if (timer != null) {
-        clearTimeout(timer);
-    }
-    var delay = noDelay ? 0 : TYPEDELAY;
-    timer = setTimeout(doSearch, delay);
-}
-
-var sameSearch = (function() {
-    var lastsearch = '';
-    return function(newsearch) {
-        //alert("newsearch: " + newsearch + "  lastsearch: " + lastsearch); 
-        var isSame = (newsearch == lastsearch);
-        lastsearch = newsearch;
-        return isSame;
-    }
-})();
-
