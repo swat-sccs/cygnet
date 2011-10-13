b55ec400348e4aa04f1b5e10b01d611008a21a24
Nick Felt <nfelt1@sccs.swarthmore.edu>
2011-10-13 03:36:34 -0700

    Cleaned up index.php text and php redirection.

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-	if ($_SERVER['SERVER_NAME'] === 'www.sccs.swarthmore.edu') // the cygnet doesn't work from this address -- the python script doesn't get executed
-	{
-		header('Location: http://cygnet.sccs.swarthmore.edu' . (isset($_REQUEST['terms']) ? '?terms=' . $_REQUEST['terms'] : ''));
-		die();
-	}
+// Redirect to the cygnet.sccs virtual host.
+if ($_SERVER['SERVER_NAME'] === 'www.sccs.swarthmore.edu') {
+   $termstring = (isset($_REQUEST['terms']) ? '?terms='.$_REQUEST['terms'] : '');
+   header('Location: http://cygnet.sccs.swarthmore.edu' . $termstring);
+   die();
+}
 ?>
 <html>
     <head>
@@ -18,22 +19,20 @@
                 <img src="media/cygnet-banner.png" width="315" height="60" />
             </div>
             <div style="margin-left: 315px; padding-left: 20px;">
-            	<p><strong>Instructions:</strong> To use the online cygnet,
-            	simply type what you're looking for in the "Find:" field below. When the online cygnet detects
-                that you've stopped typing, it will fetch the results and display them in this page. To search again,
-                just change the text in the "Find:" field, and the online cygnet will refresh the search results.</p>
-				<div><strong>Advanced:</strong> You can do a more specific search by using keywords in the search box.
-				An advanced search might look like, "first:matt address:lodges", which would return everyone named Matt
-				who lives in one of the lodges.  The legal keywords are: 
-	            <ul style="display: inline;" class="InlineList">
-	              <li>first</li>
-	              <li>last</li>
-	              <li>class</li>
-	              <li>phone</li>
-	              <li>email</li>
-	              <li>address</li>
-	            </ul>
-				</div>
+	      <div style="color: gray;"><strong>Advanced searching:</strong>
+		You can do a more specific search by using keywords in the search box.
+		An advanced search might look like, "first:matt address:lodges",
+		which would return everyone named Matt who lives in one of the lodges.
+		The accepted keywords are:
+	        <ul style="display: inline;" class="InlineList">
+	          <li>first</li>
+	          <li>last</li>
+	          <li>class</li>
+	          <!-- <li>phone</li> -->
+	          <li>email</li>
+	          <li>address</li>
+	        </ul>
+	      </div>
             </div>
         </div>
         <div id="searchbar" style="clear: both;">
@@ -51,27 +50,30 @@
             <span id="timelabel">Query took: </span>
             <span id="timevalue"></span>
           </div>
-        <div id="results" style="text-align:center; margin: 1em auto 0;">
         </div>
+
+        <div id="results">
+	  <!-- RESULTS GET INSERTED HERE -->
+	</div>
+
         <div id="footer">
-            <p>
-                The online cygnet is a service of the <a href="http://www.sccs.swarthmore.edu/">Swarthmore College Computer Society</a>.
-                Please use the site responsibly, or it may be taken down.
-            </p>
-            <p>
-                <strong>Tip:</strong> you can bookmark <a id="bookmarkurl" href="index.php">this link</a> to repeat this search.
-            </p>
-            <p>
-                Please send questions, comments, bug reports, or new/replacement pictures to the SCCS Staff, at
-                <a href="mailto:staff@sccs.swarthmore.edu">staff@sccs</a>.
-                Please crop and resize images to 105 pixels wide by 130 pixels tall before sending them to us.
-                If you would like to be removed from the online cygnet, email SCCS Staff at <a href="mailto:staff@sccs.swarthmore.edu">staff@sccs</a>.
-            </p>
-            <p>
-                Content copyright &copy;<?php echo date("Y") ?>. All rights reserved.
-                <br/><a href="http://www.sccs.swarthmore.edu/">Swarthmore College Computer Society</a>
-                <br/>500 College Ave, Swarthmore, PA 19081-1397
-            </p>
+          <p>
+	    <strong>Tip:</strong> you can bookmark
+	    <a id="bookmarkurl" href="index.php">this link</a> to repeat this search.
+	  </P>
+          <p>
+            The online cygnet is a service of the
+	    <a href="http://www.sccs.swarthmore.edu/">Swarthmore College Computer Society</a>.
+<!--            <br/>500 College Ave, Swarthmore, PA 19081-1397.-->
+	    Please use it responsibly or the site may be taken down.
+	  </p>
+	  <p>
+            Send questions, bug reports, or new/replacement pictures to the SCCS Staff
+	    at <a href="mailto:staff@sccs.swarthmore.edu">staff@sccs.swarthmore.edu</a>.
+            Please crop and resize images to 105 pixels wide by 130 pixels tall before
+	    sending them to us.  If you would like to be removed from the online Cygnet,
+	    email <a href="mailto:staff@sccs.swarthmore.edu">staff@sccs</a>.
+          </p>
         </div>
     </body>
 </html>
