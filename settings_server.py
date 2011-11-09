db5e7c93267ff224d014f5445b8b14bf317c653d
Nick Felt <nfelt1@sccs.swarthmore.edu>
2011-11-08 23:45:06 -0500

    Disabled logging on the server by default.

--- settings_server.py
+++ settings_server.py
@@ -2,6 +2,8 @@ from settings_base import *
 
 DIRECTORY_FILE = '/usr/sccs/pub/dir/current'
 
-LOGPARAMS.FILENAME = '/var/log/cygnet/cygnet.log'
-LOGPARAMS.FILESIZE_KB = 250
+# Disable logging on the server by default.
+LOGPARAMS.FILENAME = '/dev/null'
+#LOGPARAMS.FILENAME = '/var/log/cygnet/cygnet.log'
+LOGPARAMS.FILESIZE_KB = 1024
 LOGPARAMS.BACKUP_COUNT = 10
