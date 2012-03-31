3d63a3d9f3368ffe3bc963046cd04847088996eb
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-03-31 17:07:30 -0400

    make media root serve photos in dev mode

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -18,3 +19,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
