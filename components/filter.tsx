d5ee6042efb3491a75200abfee7fc2f6fd61c676
Nick Fettig <116211231+nfettig1@users.noreply.github.com>
2023-04-27 11:33:12 -0400

    minor changes to links/filters

--- components/filter.tsx
+++ components/filter.tsx
@@ -7,16 +7,31 @@ interface FilterProps {
 
 export default function Filter(props: FilterProps) {
     return(
-    <div className={props.filterOn?"position-absolute filterwindow":"position-absolute invisible"}>
+    <div className={props.filterOn?"position-absolute filterwindow":"position-absolute opacity-0 invisible"}>
         <p className="h4 font-semibold">Filters</p>
-        <div className="filterSelect shadow d-inline-block position-relative">
+        <div className="filterSelect shadow-lg d-inline-block position-relative">
             <select>
                 <option disabled selected>Dorms</option>
-                <option>a dorm</option>
+                <option>Alice Paul</option>
+                <option>Dana</option>
+                <option>Danawell</option>
+                <option>David Kemp</option>
+                <option>Hallowell</option>
+                <option>Kyle</option>
+                <option>Mertz</option>
+                <option>Mary Lyons</option>
+                <option>Palmer</option>
+                <option>Pittenger</option>
+                <option>PPR Appts</option>
+                <option>Roberts</option>
+                <option>Wharton</option>
+                <option>Willets</option>
+                <option>Woolman</option>
+                <option>Worth</option>
             </select>
             <Image className="chevron-sm position-absolute" src = {chevron} alt="" />
         </div>
-        <div className="filterSelect shadow d-inline-block position-relative">
+        <div className="filterSelect shadow-lg d-inline-block position-relative">
             <select>
                 <option disabled selected>Class Year</option>
                 <option>2023</option>
@@ -26,7 +41,7 @@ export default function Filter(props: FilterProps) {
             </select>
             <Image className="chevron-sm position-absolute" src = {chevron} alt="" />
         </div>
-        <button className="filterButton shadow">Clear</button>
+        <button className="filterButton shadow">Reset</button>
     </div>
     )
 }
