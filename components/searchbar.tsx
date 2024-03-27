9a57f392b542017e201b204752f078ffa86b5d87
Thomas Makin <halorocker89@gmail.com>
2024-03-27 00:23:07 -0400

    nav: rework for mobile

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -9,8 +9,8 @@ import { useState } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 interface SearchbarProps {
-  setSearchQuery: (query: string) => void;
-  setFilters: (query: string) => void;
+    setSearchQuery: (query: string) => void;
+    setFilters: (query: string) => void;
 }
 
 export default function SearchBar() {
@@ -21,9 +21,9 @@ export default function SearchBar() {
     function handleSearch(term: string) {
         const params = new URLSearchParams(searchParams);
         if (term) {
-          params.set('query', term);
+            params.set('query', term);
         } else {
-          params.delete('query');
+            params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
     }
@@ -31,31 +31,33 @@ export default function SearchBar() {
     function handleFilters(term: string) {
         const params = new URLSearchParams(searchParams);
         if (term) {
-          params.set('filters', term);
+            params.set('filters', term);
         } else {
-          params.delete('filters');
+            params.delete('filters');
         }
         replace(`${pathname}?${params.toString()}`);
     }
 
     const [filterOn, setFilterOn] = useState(false);
 
-  return (
-    <>
-        <div className="bg-white margin-spacing rounded-pill d-inline-flex align-items-center w-full px-4 shadow-sm py-3 position-relative">
-          <Image src = {search} alt = "search" className="search-size-g"/>
-          <input
-          type = "search"
-          className = "flex-grow-1 mx-4 mont border-0 searchbar"
-          onChange={(e) => {
-            handleSearch(e.target.value);
-          }}
-          placeholder="Search for Swarthmore College students..."
-          defaultValue={searchParams.get('query')?.toString()}/>
-          <Image src={line} alt="|" className="search-size-g" />
-          <Image src={chevron} alt="filters" onClick={()=>setFilterOn(!filterOn)} className={filterOn?"chevron-down chevron":"chevron"}/>
-          <Filter filterOn={filterOn} setFilters={handleFilters}/>
-        </div>
-    </> 
-  )
+    return (
+        <>
+            <div className="container">
+                <div className="bg-white margin-spacing rounded-pill d-inline-flex align-items-center w-full px-4 shadow-sm py-3 position-relative">
+                    <Image src={search} alt="search" className="search-size-g" />
+                    <input
+                        type="search"
+                        className="flex-grow-1 mx-4 mont border-0 searchbar"
+                        onChange={(e) => {
+                            handleSearch(e.target.value);
+                        }}
+                        placeholder="Search for Swarthmore College students..."
+                        defaultValue={searchParams.get('query')?.toString()} />
+                    <Image src={line} alt="|" className="search-size-g" />
+                    <Image src={chevron} alt="filters" onClick={() => setFilterOn(!filterOn)} className={filterOn ? "chevron-down chevron" : "chevron"} />
+                    <Filter filterOn={filterOn} setFilters={handleFilters} />
+                </div>
+            </div>
+        </>
+    )
 }
