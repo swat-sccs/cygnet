961ca9538eb6f11cf830d5f2a3187501a0bf0316
Thomas Makin <halorocker89@gmail.com>
2024-04-22 13:15:14 -0400

    pagebody: never be undefined
    Use a nice helpful message instead!

--- components/pagebody.tsx
+++ components/pagebody.tsx
@@ -144,11 +144,29 @@ export default function PageBody({
     const filteredData = filterData(searchParams);
 
     return (
-      <div className="">
         <Suspense fallback={<CardBody filteredData={undefined} />}>
           <CardBody filteredData={filteredData} />
         </Suspense>
-      </div>
     );
+  } else {
+    return (
+          <div className="mont d-flex mt-5 align-items-center justify-content-center row w-100">
+            <div className="col-12">
+                <p
+                    className="h4 text-center"
+                >
+                    Welcome to<br/>
+                    <span className="play text-black">the&nbsp;<span className="h1">CYGNET</span><span className="h4 grad"> by SCCS</span></span>
+                </p>
+            </div>
+            <div className="col-12">
+                <p
+                    className="text-center h6 mt-5"
+                >
+                    Enter a query or add a filter to begin
+                </p>
+            </div>
+        </div>
+    )
   }
 }
